ece272cddbb2a84b49105c00513956f442367b6e
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2024-03-01 20:24:23 +0100

    Remove the confusing "execInParallel" command

--- lib/src/processes.dart
+++ lib/src/processes.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:synchronized/synchronized.dart';
-
 extension<K, V> on Map<K, V> {
   Map<K, V> unmodifiableCopy() => Map<K, V>.unmodifiable(this);
 }
@@ -11,15 +9,8 @@ extension<T> on List<T> {
   List<T> unmodifiableCopy() => List<T>.unmodifiable(this);
 }
 
-final _lock = Lock();
-
 /// Runs a command in a shell.
 /// Returns a [ProcessResult] once the process has terminated.
-///
-/// Processes executed by this command are queued,
-/// and they cannot run in parallel (to preserve consistency in logs).
-/// A new process will not start until the previous ones have terminated.
-/// Use [execInParallel] to run a process in parallel.
 ///
 /// By default, the outputs will be printed on the console.
 /// If [silent] is true, nothing will be printed
@@ -41,45 +32,11 @@ Future<ProcessResult> exec(
   bool silent = false,
   Map<String, String>? environment,
 }) async {
-  return _lock.synchronized(
-    () => _exec(
-      executable,
-      arguments.unmodifiableCopy(),
-      workingDirectory,
-      silent,
-      environment?.unmodifiableCopy(),
-    ),
-  );
-}
-
-/// Runs a command in a shell,
-/// without waiting for other processes to complete.
-/// Returns a [ProcessResult] once the process has terminated.
-///
-/// Processes run by this command are executed immediately.
-/// Nothing will be printed on the console,
-/// but the outputs will still be available in [ProcessResult].
-///
-/// Use [workingDirectory] to set the working directory for the process.
-/// Note that the change of directory occurs before executing the process on some platforms,
-/// which may have impact when using relative paths for the executable and the arguments.
-///
-/// Use [environment] to set the environment variables for the process.
-/// If not set the environment of the parent process is inherited.
-/// Currently, only US-ASCII environment variables are supported
-/// and errors are likely to occur if an environment variable with code-points
-/// outside the US-ASCII range is passed in.
-Future<ProcessResult> execInParallel(
-  String executable,
-  List<String> arguments, {
-  String? workingDirectory,
-  Map<String, String>? environment,
-}) async {
   return _exec(
     executable,
     arguments.unmodifiableCopy(),
     workingDirectory,
-    true,
+    silent,
     environment?.unmodifiableCopy(),
   );
 }
